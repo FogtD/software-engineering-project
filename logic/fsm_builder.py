a36648616cb8d8db71bde97e4251e4a8f3fbaaa9
AxodenelCorvus <Zues01222@gmail.com>
2025-10-28 09:33:50 -0400

    Form transitions dictionary for constructing dfa

--- logic/fsm_builder.py
+++ logic/fsm_builder.py
@@ -1,18 +1,28 @@
+from machine_model import MachineModel
+from automata.fa.dfa import DFA
+from graph_items.edge_item import EdgeItem
+from graph_items.node_item import NodeItem
+
 class FSMBuilder(MachineModel):
     # Self will now contain a MachineModel object within it (type of inheritence)
     def build(self):
         # Get the transitions and edges to create a dictionary in the form
         # 'start state' : {'symbol': 'end state', 'symbol': 'end state'}
         # Basically you're building the transitions dictionary
-
+        
+        dfa_transitions = {}
+        for node in self.nodes:
+            curr_state_transitions = {}
+            for edge in node.edges:
+                curr_state_transitions[edge.symbol] =  edge.node2.name
+            dfa_transitions[node.name] = curr_state_transitions
+        
         dfa = DFA(
             states = self.states,
             input_symbols = self.input_symbols,
-            transitions = transitions,
+            transitions = dfa_transitions,
             initial_state = self.initial_state,
             final_states = self.final_states
             )
         return dfa
-    
-
-    
+        
